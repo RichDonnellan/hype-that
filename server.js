ad78f97c64d5af5de5a150e39e0170bd38d4b438
Ivy Wong <llovee91@Joes-MacBook-Pro.local>
2015-11-30 16:16:38 -0800

    added and views folder

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
-var express    = require('express'),
-    app        = express(),
-    logger     = require('morgan'),
+var    express = require('express'),
     bodyParser = require('body-parser'),
-    SC         = require('node-soundcloud')
+        logger = require('morgan'),
+            SC = require('node-soundcloud')
+          port = process.env.Port || 3000,
+           app = express(),
+           ejs = require('ejs'),
+    ejsLayouts = require('express-ejs-layouts'),
+     apiRouter = express.Router()
+
+
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use(express.static('/public'))
+
+app.get('/',function(req, res) {
+  res.render('index')
+})
+
+SC.init({
+  id: b0d75d37539752ace8011cf07b7e02c1,
+  secret: 5233e48846667dc6da21735d9490b308,
+  uri: 'http://localhost:3000/',
+  accessToken: 'https://api.soundcloud.com/oauth2/token'
+});
+
+// apiRouter.route('/search')
+//   .get(function(req, res) {
+//     sc.get('/tracks', {q: req.body.searchQuery}, function(err, track){
+//       if (track) {
+//         res.render('')
+//       }
+//     })
+//
+//
+//   })
+
+
+app.listen(port)
